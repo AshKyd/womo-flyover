ce30b29cac52858c479258a245df12dfe748209b
Ash Kyd <ash@kyd.com.au>
2025-04-28 11:02:09 +1000

    feature: china southern

--- utils.mjs
+++ utils.mjs
@@ -1,108 +1,103 @@
-import titlecase from 'titlecase'
+import titlecase from "titlecase";
 
 const flightCodes = {
-    SIA: 'Singapore Airlines',
-    QF: 'Qantas',
-    VOZ: "Virgin Australia",
-    MXD: 'Batik Air Malaysia',
-    UAE: 'Emirates',
-    CPA: 'Cathay Pacific',
-    CAL: 'China Airlines'
-}
+  SIA: "Singapore Airlines",
+  QF: "Qantas",
+  VOZ: "Virgin Australia",
+  MXD: "Batik Air Malaysia",
+  UAE: "Emirates",
+  CPA: "Cathay Pacific",
+  CAL: "China Airlines",
+  CSN: "China Southern",
+};
 
 export function articleise(word) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    const letter = word.slice(0, 1).toLowerCase();
-    if (vowels.includes(letter)) {
-        return `an ${word}`;
-    }
-    return `a ${word}`;
+  const vowels = ["a", "e", "i", "o", "u"];
+  const letter = word.slice(0, 1).toLowerCase();
+  if (vowels.includes(letter)) {
+    return `an ${word}`;
+  }
+  return `a ${word}`;
 }
 
-
 export function getAirline(flight) {
+  const [_code, operatorFromCode] =
+    Object.entries(flightCodes).find(
+      ([code]) => flight.flight?.slice(0, code.length) === code
+    ) || [];
 
-    const [_code, operatorFromCode] = Object.entries(flightCodes).find(([code]) => flight.flight?.slice(0, code.length) === code) || [];
+  if (operatorFromCode) {
+    return operatorFromCode;
+  }
 
-    if (operatorFromCode) {
-        return operatorFromCode;
-    }
+  if (!flight.ownOp) {
+    return "Unknown operator";
+  }
 
-    if (!flight.ownOp) {
-        return 'Unknown operator';
-    }
-
-    const operator = flight.ownOp.toUpperCase();
-    return titlecase(operator.replace(/\sPTY\sLIMITED\.?/, '').replace(/\sPTY\sLTD\.?/, '').replace(' LIMITED', '').toLowerCase());
+  const operator = flight.ownOp.toUpperCase();
+  return titlecase(
+    operator
+      .replace(/\sPTY\sLIMITED\.?/, "")
+      .replace(/\sPTY\sLTD\.?/, "")
+      .replace(" LIMITED", "")
+      .toLowerCase()
+  );
 }
 
 export function sanitiseModel(model) {
-    return titlecase(model || 'unknown aircraft');
+  return titlecase(model || "unknown aircraft");
 }
 
 export function isInBoundingBox(point, boundingBox) {
-    const [lng, lat] = point;
-    const [top, right, bottom, left] = boundingBox;
-    const isLatInbounds = lat <= top && lat >= bottom;
-    const isLonInBounds = lng <= right && lng >= left;
-    return isLatInbounds && isLonInBounds;
+  const [lng, lat] = point;
+  const [top, right, bottom, left] = boundingBox;
+  const isLatInbounds = lat <= top && lat >= bottom;
+  const isLonInBounds = lng <= right && lng >= left;
+  return isLatInbounds && isLonInBounds;
 }
 
 export function makeGeoJson(flights, bbox) {
-    const [top, right, bottom, left] = bbox;
-    return {
-        "type": "FeatureCollection",
-        "features": [...flights.flatMap(flight => [
-            (
-                {
-                    "type": "Feature",
-                    "properties": flight,
-                    "geometry": {
-                        "coordinates": [flight.lon, flight.lat],
-                        "type": "Point"
-                    }
-                }),
-            flight.lineString?.length > 1 && {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                    "coordinates": flight.lineString,
-                    "type": "LineString"
-                }
-            }
-        ].filter(Boolean)),
+  const [top, right, bottom, left] = bbox;
+  return {
+    type: "FeatureCollection",
+    features: [
+      ...flights.flatMap((flight) =>
+        [
+          {
+            type: "Feature",
+            properties: flight,
+            geometry: {
+              coordinates: [flight.lon, flight.lat],
+              type: "Point",
+            },
+          },
+          flight.lineString?.length > 1 && {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              coordinates: flight.lineString,
+              type: "LineString",
+            },
+          },
+        ].filter(Boolean)
+      ),
 
-
-        {
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-                "coordinates": [
-                    [
-                        [
-                            left,
-                            top
-                        ],
-                        [
-                            right,
-                            top
-                        ],
-                        [
-                            right,
-                            bottom
-                        ],
-                        [
-                            left,
-                            bottom
-                        ],
-                        [
-                            left,
-                            top
-                        ],
-                    ]
-                ],
-                "type": "Polygon"
-            }
-        }]
-    }
+      {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          coordinates: [
+            [
+              [left, top],
+              [right, top],
+              [right, bottom],
+              [left, bottom],
+              [left, top],
+            ],
+          ],
+          type: "Polygon",
+        },
+      },
+    ],
+  };
 }
