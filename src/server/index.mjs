0f103dc17435ad5a2002fd5da46742536942509e
Ash Kyd <ash@kyd.com.au>
2025-04-30 07:06:19 +1000

    feature: web server/invisible link

---
+++ src/server/index.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+const app = express();
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.status(401);
+  res.json({});
+});
+app.get("/robots.txt", (req, res) => {
+  res.header("content-type", "text/plain");
+  res.send(["User-agent: *", "Disallow: /"].join("\n"));
+});
+
+app.get("/go/:code", (req, res) => {
+  const code = req.params.code;
+  if (!code.match(/[a-z0-9]+/)) {
+    res.send("Invalid code");
+  }
+  res.status(302);
+  res.header("Location", `https://globe.adsb.fi/?icao=${code}`);
+  res.json({});
+});
+
+export function startServer(port = 3000) {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
